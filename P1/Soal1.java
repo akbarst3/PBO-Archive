e32c956743db46df07513fba221a2828b80b23ef
Restu Akbar <rebartheblues1905@gmail.com>
2024-08-23 07:40:21 +0700

    add: praktikum 1

---
+++ P1/Soal1.java
@@ -0,0 +1,8 @@
+public class Soal1 {
+    public static void main(String[] args) {
+        byte angkal = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angkal + angka2);
+        System.out.println("Hasil 1 " + hasil);
+    }
+}
